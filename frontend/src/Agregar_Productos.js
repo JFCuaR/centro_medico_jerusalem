3d389eb157dbf5f6336051c3867fbed7cdedb42d
JFCuaR <142605847+JFCuaR@users.noreply.github.com>
2025-05-01 22:43:32 -0600

    Update Agregar_Productos.js

--- frontend/src/Agregar_Productos.js
+++ frontend/src/Agregar_Productos.js
@@ -2,7 +2,9 @@ import React, { useState, useEffect, useContext } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import AuthContext from './AuthContext';
+import Sidebar from './sidebar'; // ← Importar 
 import './AgregarProduc.css';
+import Navbar from './Navbar'; 
 
 const URL = process.env.REACT_APP_URL_BACKEND || 'http://localhost:3001'
 
@@ -102,49 +104,12 @@ function Home() {
       />
       <div className={`d-flex ${isSidebarOpen ? 'toggled' : ''}`} id="wrapper">
         {/* Sidebar */}
-        <div className="bg-dark border-right" id="sidebar-wrapper">
-          <div className="sidebar-heading text-white"><br /><br />CENTRO MEDICO</div>
-          <div className="sidebar-heading text-white">JERUSALEM <br /><br /></div>
-          <div className="list-group list-group-flush">
-          <Link to="/Home" className="list-group-item list-group-item-action bg-dark text-white">
-              Inicio
-             </Link>
-             <Link to="/AgregarUsuario" className="list-group-item list-group-item-action bg-dark text-white">
-              Agregar Usuario
-             </Link>
-            <Link to="/Agregar_productos" className="list-group-item list-group-item-action bg-dark text-white">
-              Agregar Medicamentos
-            </Link>
-            <Link to="/ventas" className="list-group-item list-group-item-action bg-dark text-white">
-              Farmacia
-            </Link>
-            <Link to="/Devoluciones" className="list-group-item list-group-item-action bg-dark text-white">
-              Devoluciones
-            </Link>
-            <Link to="/Historial" className="list-group-item list-group-item-action bg-dark text-white">
-              Historial Medico
-            </Link>
-            <Link to="/Buscar_paciente" className="list-group-item list-group-item-action bg-dark text-white">
-              Buscar paciente
-            </Link>
-            <Link to="/Reportes" className="list-group-item list-group-item-action bg-dark text-white">
-              Reportes
-            </Link>
-          </div>          
-        </div>
+        <Sidebar isOpen={isSidebarOpen} />
         {/* /#sidebar-wrapper */}
         
         {/* Page Content */}
         <div id="page-content-wrapper">
-          <nav className="navbar navbar-expand-lg navbar-light bg-light border-bottom">
-            <button className="btn btn-primary" id="menu-toggle" onClick={toggleSidebar}>
-              Menu
-            </button>
-            <button className="btn btn-danger ml-auto" onClick={handleLogout}>
-              Cerrar Sesión
-            </button>
-          </nav>
-
+        <Navbar toggleSidebar={toggleSidebar} />
           <div className="container-fluid">
             <h2>Agregar Medicamento</h2>
             <form onSubmit={handleSubmit}>
